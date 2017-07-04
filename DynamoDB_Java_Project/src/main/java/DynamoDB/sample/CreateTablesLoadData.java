2fd925c3ad84423a87ab26af55dc368541ad8a8a
rakeshhathalia <rakeshhathalia@gmail.com>
2017-07-04 15:52:12 +0530

    table names changed

--- DynamoDB_Java_Project/src/main/java/DynamoDB/sample/CreateTablesLoadData.java
+++ DynamoDB_Java_Project/src/main/java/DynamoDB/sample/CreateTablesLoadData.java
@@ -31,10 +31,10 @@ public class CreateTablesLoadData {
     static SimpleDateFormat dateFormatter = new SimpleDateFormat(
             "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
-    static String productCatalogTableName = "ProductCatalog";
-    static String forumTableName = "Forum";
-    static String threadTableName = "Thread";
-    static String replyTableName = "Reply";
+    static String productCatalogTableName = "ProductCatalogRakesh";
+    static String forumTableName = "ForumRakesh";
+    static String threadTableName = "ThreadRakesh";
+    static String replyTableName = "ReplyRakesh";
 
     public static void main(String[] args) throws Exception {
 
